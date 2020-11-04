40c60d1b176d78b5a47ea8934b787ccf7d04f03a
andreas <andreas@wellenvogel.de>
2020-11-04 16:28:47 +0100

    add -r and -d

--- Readme.md
+++ Readme.md
@@ -17,14 +17,18 @@ Eiher install them using pip or your package manager.
 
 Usage
 -----
-    nmea-ais.py ser:/dev/ttyUSB0:9600 udp:localhost:34667 mmsi=277799911 shiptype=sail
+    nmea-ais.py -r ser:/dev/ttyUSB0:9600 udp:localhost:34667 mmsi=277799911 shiptype=sail
 or
     
-    nmea-ais.py tcp:localhost:34567 udp:localhost:34667 mmsi=277799911 shiptype=sail    
+    nmea-ais.py -r tcp:localhost:34567 udp:localhost:34667 mmsi=277799911 shiptype=sail    
 
 With the first call the program receices NMEA data from /dev/ttyUSB0 and sends out the AIS messages to localhost, port 34667.
 With the second call it receives data via tcp from localhost, port 34567.
 The AIS messages will be sent out whenever a valid NMEA position report is received.
+
+The following flags are supported:
+* -r - retry forever (e.g. when the port cannot be opened on start)
+* -d - output debug messages
 
 Supported AIS parameters
 ------------------------
@@ -36,6 +40,17 @@ The following AIS parameters are supported:
 * shiptype - refer to [the source](nmea-ais.py) for a list of supported types
 
 If the destination is not set on the commandline a string containing the current altitude will be used.
+
+Systemd unit file
+-----------------
+To support automated startup you can copy the file nmeaais.service to /etc/systemd/system and adapt it to your path and port parameters.
+Afterwards run the following commands:
+    
+    sudo systemctl daemon-reload
+    sudo systemctl enable nmeaais
+    sudo systemctl start nmeaais
+
+
 
 
 
--- nmea-ais.py
+++ nmea-ais.py
@@ -2,15 +2,19 @@
 import socket
 import sys
 #install python-bitstring (pip install bitstring)
+import time
+
 import pythonais.aislib as aislib
 #install python-nmea2 (or pip install pynmea2)
 import pynmea2
 #install python-serial (pip install pyserial)
 import serial
 
+doDebug=0
 
 def debug(txt):
-  print("##%s"%txt)
+  if doDebug > 0:
+    print("##%s"%txt)
 
 def getShiptype(options):
   typev=options.get('shiptype')
@@ -157,80 +161,109 @@ def createOutput(output):
     raise Exception("unknown output type %s"%output)
   return UdpWriter(opopt[1],opopt[2])
 
-def main(input,output,aisoptions):
-
-  reader=createReader(input)
-  writer=createOutput(output)
-  reader.open()
-  writer.open()
-  speed=0
-  course=0
-  altitude=0
+def main(input,output,aisoptions={},tryForever=False):
   while True:
     try:
-      line = reader.readline()
-      if not line.startswith("$"):
-        continue
-      msg = pynmea2.parse(line)
-      debug("msg: %s"%(type(msg)))
-      debug(repr(msg))
-      if isinstance(msg,pynmea2.RMC):
-        speed=msg.spd_over_grnd
-        course=msg.true_course
-      if isinstance(msg,pynmea2.GGA):
-        altitude=msg.altitude
-      if isinstance(msg,pynmea2.LatLonFix):
-        print("Position lat=%f,lon=%f"%(msg.latitude,msg.longitude))
-        mmsi=int(aisoptions.get('mmsi') or 333333333)
-        aismsg = aislib.AISPositionReportMessage(
-          mmsi=mmsi,
-          status=0,
-          sog=int(speed*10),
-          pa=1,
-          lon=int(msg.longitude * 600000.0),
-          lat=int(msg.latitude * 600000.0),
-          cog=int(course*10),
-          ts=0,
-          raim=1,
-          comm_state=82419
-        )
-        ais = aislib.AIS(aismsg)
-        payload = ais.build_payload(False)
-        print("!!%s"%payload)
-        writer.send(payload+"\n")
-        aismsg=aislib.AISStaticAndVoyageReportMessage(
-          mmsi=mmsi,
-          callsign=filterString(aisoptions.get('callsign')),
-          destination=filterString(aisoptions.get('destination') or "alt=%d"%int(altitude)),
-          shiptype=getShiptype(aisoptions),
-          shipname=filterString(aisoptions.get('shipname'))
-        )
-        ais = aislib.AIS(aismsg)
-        payload = ais.build_payload(False)
-        print("!!%s" % payload)
-        writer.send(payload + "\n")
-    except serial.SerialException as e:
-      print('Device error: {}'.format(e))
-      break
-    except pynmea2.ParseError as e:
-      print('Parse error: {}'.format(e))
-      continue
+      reader=createReader(input)
+      writer=createOutput(output)
+      reader.open()
+      print("opened %s"%input)
+      writer.open()
+      print("opened %s" % output)
+      speed=0
+      course=0
+      altitude=0
+      hasData=False
+      while True:
+        try:
+          line = reader.readline()
+          if not line.startswith("$"):
+            continue
+          msg = pynmea2.parse(line)
+          debug("msg: %s"%(type(msg)))
+          debug(repr(msg))
+          if isinstance(msg,pynmea2.RMC):
+            speed=msg.spd_over_grnd
+            course=msg.true_course
+          if isinstance(msg,pynmea2.GGA):
+            altitude=msg.altitude
+          if isinstance(msg,pynmea2.LatLonFix):
+            if not hasData:
+              print("Position lat=%f,lon=%f"%(msg.latitude,msg.longitude))
+              hasData=True
+            else:
+              debug("Position lat=%f,lon=%f"%(msg.latitude,msg.longitude))
+            mmsi=int(aisoptions.get('mmsi') or 333333333)
+            aismsg = aislib.AISPositionReportMessage(
+              mmsi=mmsi,
+              status=0,
+              sog=int(speed*10),
+              pa=1,
+              lon=int(msg.longitude * 600000.0),
+              lat=int(msg.latitude * 600000.0),
+              cog=int(course*10),
+              ts=0,
+              raim=1,
+              comm_state=82419
+            )
+            ais = aislib.AIS(aismsg)
+            payload = ais.build_payload(False)
+            debug("!!%s"%payload)
+            writer.send(payload+"\n")
+            aismsg=aislib.AISStaticAndVoyageReportMessage(
+              mmsi=mmsi,
+              callsign=filterString(aisoptions.get('callsign')),
+              destination=filterString(aisoptions.get('destination') or "alt=%d"%int(altitude)),
+              shiptype=getShiptype(aisoptions),
+              shipname=filterString(aisoptions.get('shipname'))
+            )
+            ais = aislib.AIS(aismsg)
+            payload = ais.build_payload(False)
+            debug("!!%s" % payload)
+            writer.send(payload + "\n")
+        except serial.SerialException as e:
+          print('Device error: {}'.format(e))
+          break
+        except pynmea2.ParseError as e:
+          print('Parse error: {}'.format(e))
+          continue
+        except Exception as e:
+          print('general error: {}'.format(e))
+          continue
     except Exception as e:
-      print('general error: {}'.format(e))
-      continue
+      print("exception: %s"%e)
+      if not tryForever:
+        raise
+      time.sleep(2)
 
 
 if __name__ == '__main__':
-  if len(sys.argv) < 3:
-    print("usage: %s input output [mmsi=...] [shipname=...]..."%sys.argv[0])
+  aisopts={}
+  args=[]
+  args.extend(sys.argv)
+  tryForever=False
+  if len(args) > 0:
+    args.pop(0)
+  while len(args) > 0 and args[0][0] == '-':
+    flag=args.pop(0)
+    if flag == '-d':
+      doDebug=1
+      continue
+    if flag == '-r':
+      tryForever=True
+      continue
+    print("unknown arg %s",flag)
+  if len(args) < 2:
+    print("usage: %s [-d] [-r] input output [mmsi=...] [shipname=...]..."%sys.argv[0])
     print("          input: ser:port:baud or tcp:host:port")
     print("          output: udp:host:port")
+    print("          -d: print debug messages")
+    print("          -r: retry forever")
     sys.exit(1)
-  aisopts={}
-  for a in sys.argv[3:]:
+  for a in args[2:]:
     nv=a.split("=")
     if len(nv) != 2:
       print("invalid arg %s"%a)
       sys.exit(1)
     aisopts[nv[0]]=nv[1]
-  main(sys.argv[1],sys.argv[2],aisopts)
+  main(args[0],args[1],aisopts,tryForever=tryForever)
