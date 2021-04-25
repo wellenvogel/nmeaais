f61d1ff1fbec1a8358e34ff8ebc6c35414ad9219
andreas <andreas@wellenvogel.de>
2021-04-25 11:22:12 +0200

    add a second predict link

--- avnav/plugin.js
+++ avnav/plugin.js
@@ -90,6 +90,9 @@
   </div>`;
     if (link){
       rt+=`
+      <div class="linkRow">
+      <a onclick="clickLink" href="#" class="extLink" target="predict" >Predict</a>
+    </div>
     <div class="predictRow">
       <button onclick="click" class="predict" >Predict</button>
     </div>
@@ -126,18 +129,16 @@
       var latitude=current.lat;
       var link=undefined;
       if (launch_alt !== undefined){
-        if (launch_alt < 0) launch_alt=1000; //TODO: test
+        var burstAlt=launch_alt-7;
         //we can only compute the link if we already have previous
         //altitude and time values
         //they are stored at this.lastAlt and this.lastTime
         //if they are not set, the link is unset (and the button will not be shown)
         if (this.lastAlt !== undefined && this.lastTime){
           var tdiff=now.getTime()-this.lastTime;
-          var altDiff=launch_alt-this.lastAlt;
-          var burstAlt=launch_alt-7;
+          var altDiff=this.lastAlt-launch_alt;
           if (tdiff > 0){
             var downRate=altDiff*1000/tdiff;
-            if (downRate < 1) downRate=7; //TODO: test
             link=`http://predict.cusf.co.uk/api/v1/?launch_latitude=${latitude}&launch_longitude=${longitude}&`+
             `launch_altitude=132.1&launch_datetime=${launch_time}&ascent_rate=15&burst_altitude=${burstAlt.toFixed(1)}&descent_rate=${downRate}`;
             this.link=link; //store the link to be used later on in the button click
@@ -157,33 +158,53 @@
       var displayFrame=document.getElementById(frameId);
       if (! displayFrame){
         //create the display elements as they are not there
-        displayFrame=document.createElement('iframe');
+        displayFrame=document.createElement('div');
         displayFrame.setAttribute('id',frameId);
         var wrapper=document.createElement('div');
         wrapper.setAttribute('id','aisSondeFrameWrapper');
         wrapper.addEventListener('click',function(){
           //when we click on the grey frame around the display we hide it
-          displayFrame.classList.remove('visible');
+          wrapper.classList.remove('visible');
         })
         wrapper.appendChild(displayFrame);
         //insert our display elemnt to the document
         document.body.appendChild(wrapper);
       }
-      //remember our display frame
+      //remember our display frame and the wrapepr around
       context.displayFrame=displayFrame;
+      context.displayFrameWrapper=displayFrame.parentElement;
       //action for predict button click
       context.eventHandler.click=function(){
         if (context.displayFrame && context.link){
           //set the source of the display element to the last computed link
-          context.displayFrame.src=context.link;
-          //show the display
-          context.displayFrame.classList.add('visible');
+          fetch(context.link)
+            .then(function(resp){return resp.json()})
+            .then(function(jdata){
+              //here all the data is available as json
+              //you could also create some text/html out of the json object
+              //if you want to set HTML just use innerHTML instead of textContent
+              context.displayFrame.textContent=JSON.stringify(jdata,undefined,2);
+              //show the display
+              context.displayFrameWrapper.classList.add('visible');
+            })
+            .catch(function(error){
+              avnav.api.showToast(error);
+            })
         }
+      }
+      //sometimes the prediction will just return an error
+      //but we cannot really display it (CORS)
+      //so we have a second link that will really load the page - leaving AvNav
+      context.eventHandler.clickLink=function(ev){
+        ev.preventDefault();
+        if (context.link) window.location.href=context.link;
       }
     },
     finalizeFunction:function(context){
       //if the widget goes away - just hide our display
-      if (context.displayFrame) context.displayFrame.classList.remove('visible');
+      if (context.displayFrameWrapper) {
+        context.displayFrameWrapper.classList.remove('visible');
+      }
     },
     storeKeys:{
       all:'nav.ais.list'
